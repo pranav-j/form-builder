77c4d7b3b2531d9bcb7fa7014f7e971910411930
pranav-j <pranavj10@gmail.com>
2024-12-17 06:45:40 +0530

    added a few input types

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,91 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 
 function App() {
-  const inputsTypes = ["number", "text", "checkbox", "radio", "select"];
-  const [formTitle, setFormTitle] = useState("");
+	const [inputType, setInputType] = useState("");
+	const [inputTypeAttributes, setInputTypeAttributes] = useState({type: inputType});
+	const [fields, setFields] = useState([]);
+	const [formTitle, setFormTitle] = useState("");
 
-  return (
-    <div className="m-5">
+	const attributeMap = {
+		text: ["label", "name", "required", "placeholder"],
+		number: ["label", "name", "required", "min", "max"],
+	};
+
+	const handleAttributes = (key, value) => {
+		setInputTypeAttributes((prev) => ({
+			...prev,
+			[key]: value
+		}))
+	};
+
+	const addField = () => {
+		setFields((prev) => [...prev, inputTypeAttributes]);
+		setInputTypeAttributes({});
+	};
+
+	useEffect(() => {
+		// console.log(inputTypeAttributes);
+		// console.log(fields);
+		const formData = {
+			title: formTitle,
+			fields
+		};
+		console.log(formData);
+	}, [inputTypeAttributes, fields])
+
+	return (
+	<div className="m-5">
 		<h1 className="border rounded-2xl flex justify-center items-center font-bold text-blue-500 mb-5">FORM BUILDER</h1>
 
 		<div className="flex gap-5">
-			<div className="w-1/2 border rounded-2xl p-10">
-				<div className="flex flex-col gap-3 mb-5">
-				{inputsTypes.map((inputsType, idx) => (
-					<button className=" border-none rounded-md w-fit px-3 bg-blue-200" key={idx} >{inputsType}</button>
-				))}
-				</div>
+			<div className="w-1/2 flex border rounded-2xl p-10">
+				<div className="w-1/3 flex flex-col gap-3 mb-5">
+					<button className="border-none rounded-md w-fit px-3 bg-blue-200" 
+						onClick={() => {
+							setInputType("text");
+							setInputTypeAttributes({type: "text"})
+						}
+					}>
+						text
+					</button>
 
-				<button className="bg-blue-500 border-none rounded-md px-3">Create a form</button>
+					<button className="border-none rounded-md w-fit px-3 bg-blue-200" 
+						onClick={() => {
+							setInputType("number");
+							setInputTypeAttributes({type: "number"})
+						}
+					}>
+						number
+					</button>
+				</div>
+				{inputType && 
+				<div className="w-2/3 flex flex-col justify-center gap-3">
+					
+						<div className="flex flex-col gap-2 border rounded-md p-3">
+							{attributeMap[inputType].map((attribute, idx) => (
+								<div key={idx}>
+									{attribute !== "required" && 
+										<div>
+											<label htmlFor="">{attribute} : </label>
+											<input className="border rounded" type="text" value={inputTypeAttributes[attribute] || ""} onChange={(e) => handleAttributes(attribute, e.target.value)} />
+										</div>
+									}
+									{attribute === "required" && 
+										<div>
+											<label htmlFor="">{attribute} : </label>
+											<input className="border rounded" type="checkbox" checked={!!inputTypeAttributes[attribute]} onChange={(e) => handleAttributes(attribute, e.target.value)} />
+										</div>
+									}
+								</div>
+							))}
+						</div>
+					
+					<button className="border-none bg-green-200 px-2 py-1 rounded-md w-fit" onClick={addField}>Add this input type</button>
+				
+				</div>
+				}
+
 			</div>
 
 			<div className="w-1/2 border rounded-2xl p-10">
@@ -28,8 +96,8 @@ function App() {
 		</div>
 
 
-    </div>
-  );
+	</div>
+	);
 }
 
 export default App;
