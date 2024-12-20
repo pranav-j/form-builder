6f7c37eb68867e5c9e6ad157f2a39bdd478864af
pranav-j <pranavj10@gmail.com>
2024-12-20 10:20:46 +0530

    handled inputs

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -2,34 +2,38 @@ import { useEffect, useState } from "react";
 
 const RenderForm = ({formFields}) => {
 
-    const [formData, setFormData] = useState(() => {
-		const initialData = {};
-		formFields.fields?.forEach((field) => {
-			initialData[field] = "";
-		})
-		return initialData;
-	})
+    const [formData, setFormData] = useState({});
+
+    useEffect(() => {
+        const initialData = {};
+        formFields.fields?.forEach((field) => {
+            initialData[field.name] = "";
+        });
+        setFormData(initialData);
+    }, [formFields])
 
     useEffect(() => {
         console.log(formData);
     }, [formData])
-    
-    const printFormData = () => {
-        console.log(formData);
+
+    const handleInput = (e, fieldName) => {
+        setFormData((prev) => ({
+            ...prev,
+            [fieldName]: e.target.value
+        }))
     }
 
     return(
         <div className="w-1/2 border rounded-2xl p-10">
             <h1 className="font-semibold">{formFields.title}</h1>
-            <form action="">
+            <form action="" className="flex flex-col gap-2">
                 {formFields.fields?.map((field, idx) => (
                     <div key={idx}>
                         <label className="pr-3" htmlFor={field.name}>{field.label} :</label>
-                        <input className="border rounded-md" type={field.type} />
+                        <input onChange={(e) => handleInput(e, field.name)} className="border rounded-md" type={field.type} />
                     </div>
                 ))}
             </form>
-            <button onClick={printFormData}>Print formData</button>
         </div>
     )
 }
